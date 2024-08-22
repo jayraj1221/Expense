c55f3b31fbfc144a46bda660ab44f8966bb9f880
Jay Radadiya <jayrajpatel1221@gmail.com>
2024-08-22 22:42:14 +0530

    second commit

--- Expense/settings.py
+++ Expense/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ExpenseTracker',
-    'bootstrap5',
+    'bootstrap4',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
